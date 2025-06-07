5cba5d9f79e8b915cf60db7affd08f34b98f3ad6
Arturo Esquivel Alvarado <earturosalvador@gmail.com>
2025-06-07 17:03:15 -0500

    Arreglos en Inicio y loader

--- MAQUIRENTAPP/app/src/main/java/com/example/maquirentapp/MainActivity.java
+++ MAQUIRENTAPP/app/src/main/java/com/example/maquirentapp/MainActivity.java
@@ -3,12 +3,9 @@ package com.example.maquirentapp;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.example.maquirentapp.View.Cge;
 
 public class MainActivity extends BaseActivity {
 
@@ -16,8 +13,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.CGE), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
--- MAQUIRENTAPP/app/src/main/java/com/example/maquirentapp/View/Loader.java
+++ MAQUIRENTAPP/app/src/main/java/com/example/maquirentapp/View/Loader.java
@@ -10,45 +10,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.maquirentapp.BaseActivity;
 import com.example.maquirentapp.MainActivity;
 import com.example.maquirentapp.R;
 
-public class Loader extends BaseActivity {
-    @Override
-    protected int getHeaderIcon() {
-        return R.drawable.icon_home_blanco;
-    }
-
-    @Override
-    protected String getHeaderTitle() {
-        return "Inicio";
-    }
-
-    @Override
-    protected int getContentLayoutId() {
-        return R.layout.ly_inicio;
-    }
+public class Loader extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.ly_loader);
+        setContentView(R.layout.ly_loader);
 
-        // Espera 3 segundos antes de lanzar la actividad principal
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                Intent intent = new Intent(Loader.this, MainActivity.class);
-//                startActivity(intent);
-//                finish();
-//            }
-//        }, 3000);
-        setupMainContent();
-    }
-    private void setupMainContent() {
-        // Configuración específica de MainActivity
-        // Puedes acceder a las vistas usando getContentContainer()
+//         Espera 1.5 segundos antes de lanzar la actividad principal
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(Loader.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 1500);
     }
 
 
