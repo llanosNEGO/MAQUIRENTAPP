c97938afa0dd9a1239d03820fdd46c476f44f552
El-Paulinho <pauloyepezmessi@gmail.com>
2025-07-11 16:11:01 -0500

    Agrege el diseño del nuevoAmntenimiento

---
+++ MAQUIRENTAPP/app/src/main/java/com/example/maquirentapp/View/NuevoMantenimiento.java
@@ -0,0 +1,22 @@
+package com.example.maquirentapp.View;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.maquirentapp.R;
+
+public class NuevoMantenimiento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_nuevo_mantenimiento);
+
+    }
+}
