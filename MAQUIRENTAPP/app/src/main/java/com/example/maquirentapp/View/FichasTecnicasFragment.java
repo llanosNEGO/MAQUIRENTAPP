c41255a618f24630cd035bd1ca9f5596aed7d3f5
Abed Llanos <negolloverallanos@gmail.com>
2025-07-12 02:19:25 -0500

    agregue funcion a Ficha Tecnica

--- MAQUIRENTAPP/app/src/main/java/com/example/maquirentapp/View/FichasTecnicasFragment.java
+++ MAQUIRENTAPP/app/src/main/java/com/example/maquirentapp/View/FichasTecnicasFragment.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,7 +24,6 @@ import com.example.maquirentapp.ViewModel.FichaTecnicaViewModel;
 import com.example.maquirentapp.adaptadores.FichasTecnicasAdapter;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class FichasTecnicasFragment extends Fragment {
     private FichaTecnicaViewModel viewModel;
